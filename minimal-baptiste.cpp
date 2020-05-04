43b5a526fddbfeb2a5809332ea8b898062c5f730
Metaturgiste <metaturgie@gmail.com>
2020-05-04 14:26:22 +0200

    minimal

---
+++ minimal-baptiste.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib> // Pour atoi
+//#include <Zydis/Zydis.h>
+//#include "Dossier-git-zydis/include/Zydis/Zydis.h"
+#include "../zydis/include/Zydis/Zydis.h"
+
+using namespace std;
+
+// Éventuellement tu pourras tester en déclarant la variable instruction en dehors,
+// et en la donnant en argument (par référence ou pointeur). Pour éviter d'en créer une à
+// chaque fois : peut-être que ça ira plus vite (mais je suis dubitatif).
+inline bool isInstructionValid(unsigned char* bytes, int size, ZydisDecoder *decoder)
+{
+    ZydisDecodedInstruction instruction;
+    ZyanStatus status = ZydisDecoderDecodeBuffer(decoder, bytes, size, &instruction);
+
+    if (ZYAN_SUCCESS(status))
+        return false;
+    else
+        return true;
+}
+
+
+int main(int argc, char* argv[])
+{
+    bool zydisMinimalMode = false;
+
+    cin.tie(NULL); // ne flushe pas cout avant un cin: rend les I/O plus rapides
+    ios::sync_with_stdio(false); // désynchronine cin de stdin et cout de stdout, idem
+
+    ZydisDecoder decoder;
+    if (!ZYAN_SUCCESS(ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_COMPAT_32, ZYDIS_ADDRESS_WIDTH_32)))
+    {
+        cout << "ERROR initialising the decoder" << endl;
+        exit(1);
+    }
+    //if (!ZYAN_SUCCESS(ZydisDecoderEnableMode(&decoder, ZYDIS_DECODER_MODE_MINIMAL, zydisMinimalMode)))
+
+
+    /* Petit code pour détecter les préfixes, pour plus tard
+     *
+       vector<unsigned char> prefixes[5] = {{0xF0}, {0xF2, 0xF3}, {0x2E, 0x36, 0x3E, 0x26, 0x64, 0x65}, {0x66}, {0x67}};
+       bool isPrefix[256];
+       int prefixGroup[256];
+       for (int i = 0; i < 5; i++)
+       for (int x : prefixes[i])
+       prefixGroup[x] = i;
+
+       for (int i = 0; i < 256; i++)
+       isPrefix[i] = false;
+       for (auto &x : prefixes)
+       for (unsigned char y : x)
+       isPrefix[y] = true;
+    */
+
+    if (argc < 2)
+    {
+        cout << "Error: give the length of the word to enumerate (number of bytes)" << endl;
+        exit(1);
+    }
+
+    /* Exemple de comment utiliser les arguments. Ici on aura besoin par exemple de la fonction C atoi pour obtenir l'entier
+    if (argc > 2)
+    {
+        switch (argv[1][1])
+        {
+            case 'c':
+                runCapstone = true;
+                break;
+            case 'z':
+                runZydis = true;
+                break;
+            default:
+                runAutomaton = true;
+        }
+    }
+    */
+
+    return 0;
+}
